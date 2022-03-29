0ada4a21b62d900a2cad71bdaf24524a93040cec
kaitlynhop <kaitlyn.hopkins4@gmail.com>
2022-03-28 20:12:17 -0500

    Adding import of Data .sql code

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -35,23 +35,43 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE dept_emp (
-	emp_no INT NOT NULL,
-	dept_no VARCHAR(4) NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no, dept_no)
-);
+--CREATE TABLE dept_emp (
+--	emp_no INT NOT NULL,
+--	dept_no VARCHAR(4) NOT NULL,
+--	from_date DATE NOT NULL,
+--	to_date DATE NOT NULL,
+--	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+--	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+--	PRIMARY KEY (emp_no, dept_no)
+--);
+
+-- CREATE TABLE titles (
+	--emp_no INT NOT NULL,
+	--title VARCHAR NOT NULL,
+	--from_date DATE NOT NULL,
+	--to_date DATE NOT NULL,
+	--FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	--PRIMARY KEY (emp_no, title)
+--);
+
+DROP TABLE titles CASCADE;
 
 CREATE TABLE titles (
 	emp_no INT NOT NULL,
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no, title)
+	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no)
 );
 
-SELECT * FROM departments;
+SELECT * FROM dept_emp;
+DROP TABLE dept_emp CASCADE;
+
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
+);
